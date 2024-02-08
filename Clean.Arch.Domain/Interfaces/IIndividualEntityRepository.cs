501c35935e9f53943a718106c3213f7e55997e38
José Vitor <jose81073175@gmail.com>
2024-02-07 23:04:02 -0300

    refactore: add remove all and fix repository

--- Clean.Arch.Domain/Interfaces/IIndividualEntityRepository.cs
+++ Clean.Arch.Domain/Interfaces/IIndividualEntityRepository.cs
@@ -7,7 +7,8 @@ public interface IIndividualEntityRepository
     Task<List<IndividualEntity>> GetAll();
     Task<IndividualEntity> GetById(int id);
     Task<IndividualEntity> Insert(IndividualEntity entity);
-    Task<IndividualEntity> UpdateById(int id);
+    Task<IndividualEntity> Update(IndividualEntity entity);
     Task<List<IndividualEntity>> UpdateAll(List<IndividualEntity> entities);
-    Task<IndividualEntity> Delete(int id);
+    Task<IndividualEntity> Delete(IndividualEntity entity);
+    Task<List<IndividualEntity>> DeleteAll(List<IndividualEntity> entities);
 }
