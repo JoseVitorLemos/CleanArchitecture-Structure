276834fee2e10ab35bceb8c45a3b25c49ec1a460
José Vitor <jose81073175@gmail.com>
2024-02-10 17:00:43 -0300

    chore: create utils to return connectionstring and assembly location

---
+++ Clean.Arch.Helpers/Utils/AssemblyHelpers.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+
+namespace Clean.Arch.Helpers.Utils;
+
+public static class AssemblyHelpers
+{
+    public static string? AssemblyDirectory
+    {
+        get
+        {
+            string codeBase = Assembly.GetExecutingAssembly().Location ?? string.Empty;
+            UriBuilder uri = new UriBuilder(codeBase);
+            string path = Uri.UnescapeDataString(uri.Path);
+            return Path.GetDirectoryName(path);
+        }
+    }
+}
---
+++ Clean.Arch.Helpers/Utils/InfraHelpers.cs
@@ -0,0 +1,19 @@
+using System.Configuration;
+
+namespace Clean.Arch.Helpers.Utils;
+
+public static class InfraHelpers
+{
+    public static string? GetConnectionString()
+    {
+        string? provider = ConfigurationManager.AppSettings["provider"];
+        if (string.IsNullOrEmpty(provider))
+            throw new NullReferenceException("Invalid provider on app.config");
+
+        string? connectionString = ConfigurationManager.ConnectionStrings[provider]?.ConnectionString;
+        if (string.IsNullOrEmpty(connectionString))
+            throw new NullReferenceException("Invalid connectionString on app.config");
+
+        return provider;
+    }
+}
