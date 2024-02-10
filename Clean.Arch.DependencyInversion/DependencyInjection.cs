7b2a1f52ead6610540557eb67b4f108aee28cb86
José Vitor <jose81073175@gmail.com>
2024-02-10 17:11:08 -0300

    chore: rename project DIP

---
+++ Clean.Arch.DependencyInversion/DependencyInjection.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using Clean.Arch.Data.DatabaseContext;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Clean.Arch.Helpers.Utils;
+
+namespace Clean.Arch.DependencyInversion;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfraInjection(this IServiceCollection services,
+                                                       IConfiguration configuration)
+    {
+        services.AddDbContext<DataContext>(options => options.UseSqlServer(InfraHelpers.GetConnectionString(),
+                                           x => x.MigrationsAssembly(typeof(DbContext).Assembly.FullName)));
+
+        var classes = Assembly.Load("Clean.Arch.Data")
+            .GetTypes().Where(c => c.IsClass && !c.IsAbstract && !c.IsGenericType && c.IsPublic);
+
+        foreach (var classType in classes)
+        {
+            var interfaces = Assembly.Load("Clean.Arch.Domain")
+                .GetTypes().Where(i => i.IsInterface && i.IsAssignableFrom(classType));
+
+            foreach (var interfaceType in interfaces)
+                services.AddScoped(interfaceType, classType);
+        }
+
+        return services;
+    }
+}
