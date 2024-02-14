e6e6c1f22a09c5371ea371a020aded68edae1681
José Vitor <jose81073175@gmail.com>
2024-02-14 15:10:55 -0300

    chore: implements business layer

---
+++ Clean.Arch.Business/Business/IndividualEntityBusiness/IndividualEntityBusiness.cs
@@ -0,0 +1,29 @@
+﻿using Clean.Arch.Domain.Entities;
+using Clean.Arch.Domain.Interfaces;
+
+namespace Clean.Arch.Business.IndividualEntityBusiness;
+
+public class IndividualEntityBusiness : IIndividualEntityBusiness
+{
+    private readonly IIndividualEntityRepository _individualEntityRepository;
+
+    public IndividualEntityBusiness(IIndividualEntityRepository individualEntityRepository)
+    {
+        _individualEntityRepository = individualEntityRepository;
+    }
+
+    public async Task<IndividualEntity> GetIndividualEntity(Guid id)
+        => await _individualEntityRepository.GetById(id);
+
+    public async Task InsertIndividualEntity(IndividualEntity entity)
+        => await _individualEntityRepository.Insert(entity);
+
+    public async Task<List<IndividualEntity>> ListIndividualEntity()
+        => await _individualEntityRepository.GetAll();
+
+    public async Task RemoveIndividualEntity(Guid id)
+        => await _individualEntityRepository.Delete(id);
+
+    public async Task UpdateIndividualEntity(IndividualEntity entity)
+        => await _individualEntityRepository.Update(entity);
+}
---
+++ Clean.Arch.Business/Business/IndividualEntityBusiness/Interfaces/IIndividualEntityBusiness.cs
@@ -0,0 +1,12 @@
+﻿using Clean.Arch.Domain.Entities;
+
+namespace Clean.Arch.Business.IndividualEntityBusiness;
+
+public interface IIndividualEntityBusiness
+{
+    Task<List<IndividualEntity>> ListIndividualEntity();
+    Task<IndividualEntity> GetIndividualEntity(Guid id);
+    Task InsertIndividualEntity(IndividualEntity entity);
+    Task UpdateIndividualEntity(IndividualEntity entity);
+    Task RemoveIndividualEntity(Guid id);
+}
